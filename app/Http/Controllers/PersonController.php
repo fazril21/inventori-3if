3bfd131328f44187ce0e3978fbac695b3694e19c
Fazril Fachrezi <fazrilramdani6@gmail.com>
2021-11-16 18:49:43 +0700

    Sesi7

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -9,12 +9,29 @@ class PersonController extends Controller
     private $name = "Fazril Fachrezi Ramdani";
     private $code = "190914022";
 
+    public function sendData() {
+        $code = $this->code;
+        $name = $this->name;
+
+        return view("person.sendData", compact('code', 'name'));
+    }
+
     public function index() {
-        return $this->name . " " . $this->code ;
+        return view("person.index") ;
         
     }
     public function show($param){
         $this->name=$param;
         return $this->name;
+
+    }public function add(){
+        return view ('person.add');
+
+    }public function addProcess(Request $request){
+        $this->validate($request,[
+            'person_name' => 'required|max:30',
+            'person_email' => 'required|email'
+        ]);
+        return view('person.show',['data' => $request]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,11 @@ Route::get('/name/{nrp}/{name}', function ($nrp,$name) {
 Route::get('/person', 'PersonController@index');
 Route::get('/person/show/{param}', 'PersonController@show');
 Route::resource('/student', 'StudentController');
+Route::get('/person/sendData', 'PersonController@sendData');
+Route::get('/student', 'StudentController@index');
+Route::get('/student/sendData', 'StudentController@sendData');
+Route::get('/person/add', 'PersonController@add');
+Route::post('/person/addProcess', 'PersonController@addProcess');
+
 
 Route::get('my-academic/{course}/{task}/{quiz}/{mid_term}/{final}','StudentController@myCourse');
